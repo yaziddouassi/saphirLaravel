958463b61a663d5fd25db07e59d524e447bc5b2c
yaziddouassi <rabahdouassi@yahoo.fr>
2024-12-23 21:08:06 +0100

    Your message here

---
+++ resources/views/darkmode/multipleAudio.blade.php
@@ -0,0 +1,51 @@
+<div class="w-full"
+            x-data="{ isUploading: false, progress: 0 }"
+            x-on:livewire-upload-start="isUploading = true"
+            x-on:livewire-upload-finish="isUploading = false"
+            x-on:livewire-upload-error="isUploading = false"
+            x-on:livewire-upload-progress="progress = $event.detail.progress">
+
+           
+
+            <div class="mb-[5px]">
+                <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
+             </div>
+        
+            <div class="w-[100%]  flex items-center justify-center">
+                <label class="w-[100%]">
+                    <input type="file" wire:model="saphirMultipleFiles.{{$file}}" accept="audio/*" hidden />
+                    <div class="flex w-[100%] h-[50px] px-2 flex-col bg-[#ccc] rounded-full shadow text-[darkblue] text-[14px] font-semibold leading-4 items-center justify-center cursor-pointer focus:outline-none">Add Audio</div>
+                  </label>
+            </div>
+        
+           
+            @foreach ($saphirMultipleFiles[$file] as $key => $item)
+            <div class="pt-[5px] flex">
+                <div class="w-full">
+                    @if ($item) 
+                    <audio controls class="pt-[8px]  w-full" src="{{ $item->temporaryUrl()}}">
+                    </audio>
+                     @endif
+                </div>
+                <div class="pt-[14px]">
+                    <span class="material-icons text-[red]
+                     text-[30px] cursor-pointer"
+                     wire:click="saphirDeleteFileByKey('{{$file}}','{{$key}}')">
+                        delete_forever
+                    </span>
+                </div>
+           </div>
+         @endforeach
+        
+         @error("saphirMultipleFiles.$file")
+         <div class="text-[red] pt-[5px]">
+              <span class="error">{{ $message }}</span> 
+         </div>
+         @enderror      
+     
+            <!-- Progress Bar -->
+            <div x-show="isUploading">
+                <progress max="100" x-bind:value="progress" class="h-[10px] bg-[blue] rounded-[5px]"></progress>
+            </div>
+        
+</div>
---
+++ resources/views/darkmode/multipleImage.blade.php
@@ -0,0 +1,50 @@
+<div class="w-full"
+            x-data="{ isUploading: false, progress: 0 }"
+            x-on:livewire-upload-start="isUploading = true"
+            x-on:livewire-upload-finish="isUploading = false"
+            x-on:livewire-upload-error="isUploading = false"
+            x-on:livewire-upload-progress="progress = $event.detail.progress">
+
+           
+
+            <div class="mb-[5px]">
+                <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
+             </div>
+        
+            <div class="w-[100%]  flex items-center justify-center">
+                <label class="w-[100%]">
+                    <input type="file" wire:model="saphirMultipleFiles.{{$file}}" accept="image/png,image/jpeg" hidden />
+                    <div class="flex w-[100%] h-[50px] px-2 flex-col bg-[#ccc] rounded-full shadow text-[darkblue] text-[14px] font-semibold leading-4 items-center justify-center cursor-pointer focus:outline-none">Add Image</div>
+                  </label>
+            </div>
+        
+           
+            @foreach ($saphirMultipleFiles[$file] as $key => $item)
+            <div class="pt-[5px] flex">
+                <div class="w-full">
+                    @if ($item)  
+                        <img src="{{ $item->temporaryUrl() }}" width="100%" class="max-h-[50vh]">    
+                     @endif
+                </div>
+                <div class="">
+                    <span class="material-icons text-[red]
+                     text-[30px] cursor-pointer"
+                     wire:click="saphirDeleteFileByKey('{{$file}}','{{$key}}')">
+                        delete_forever
+                    </span>
+                </div>
+           </div>
+         @endforeach
+        
+         @error("saphirMultipleFiles.$file")
+         <div class="text-[red] pt-[5px]">
+              <span class="error">{{ $message }}</span> 
+         </div>
+         @enderror     
+
+            <!-- Progress Bar -->
+            <div x-show="isUploading">
+                <progress max="100" x-bind:value="progress" class="h-[10px] bg-[blue] rounded-[5px]"></progress>
+            </div>
+        
+</div>
