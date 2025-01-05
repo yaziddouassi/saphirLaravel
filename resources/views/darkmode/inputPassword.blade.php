a660af5ee5be7319a524b1492c6fae7007de6586
yaziddouassi <rabahdouassi@yahoo.fr>
2025-01-05 15:08:15 +0100

    Your message here

--- resources/views/darkmode/inputPassword.blade.php
+++ resources/views/darkmode/inputPassword.blade.php
@@ -1,22 +1,26 @@
 <div x-data="{ showPassword: false }" class="w-full">
     <div class="mb-[5px]">
-       <span class="text-[white] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
+       <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
     </div>
     
 
-    <div class="flex w-full bg-[#E8E8E8] h-[50px]">
+    <div class="flex w-full bg-[#E8E8E8] h-[50px] border-[1px] border-[darkblue]">
         <div class="w-full pt-[4px]">
              <input :type="showPassword ? 'text' : 'password'"  
              class="w-full bg-[#E8E8E8] h-[42px] border-[0px]
              border-transparent focus:border-transparent focus:ring-0" 
-             wire:model="saphirFields.{{$field}}">
+             wire:model="saphirPasswords.{{$field}}" autocomplete="off"
+             readonly
+             onfocus="this.removeAttribute('readonly');" >
         </div>
         <div @click="showPassword = !showPassword" class="p-[6px] pt-[12px]">
-            <i class="fa fa-eye text-[24px] text-[darkblue]"></i>
+            <span class="material-icons text-[darkblue]">
+                visibility
+                </span>
         </div>
     </div>
 
-    @error("saphirFields.$field")
+    @error("saphirPasswords.$field")
     <div class="text-[red] pt-[5px]">
          <span class="error">{{ $message }}</span> 
     </div>
