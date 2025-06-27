bb75bd2e4a703853ce3c722d1ea17f8bfcfba0fd
yaziddouassi <37118932+yaziddouassi@users.noreply.github.com>
2025-06-27 15:39:23 +0200

    Update multipleVideoEdit.blade.php

--- resources/views/darkmode/multipleVideoEdit.blade.php
+++ resources/views/darkmode/multipleVideoEdit.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full"
-            x-data="{ isUploading: false, progress: 0 }"
+            x-data="{ isUploading: false, progress: 0 ,  lastVideoUrl: null}"
             x-on:livewire-upload-start="isUploading = true"
             x-on:livewire-upload-finish="isUploading = false"
             x-on:livewire-upload-error="isUploading = false"
@@ -8,19 +8,33 @@
            
 
             <div class="mb-[5px]">
-                <span class="text-[white] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
+                <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
              </div>
         
             <div class="w-[100%]  flex items-center justify-center">
                 <label class="w-[100%]">
-                    <input type="file" wire:model="saphirMultipleFiles.{{$file}}" accept="video/*" hidden />
+                    <input type="file" wire:model="saphirMultipleFiles.{{$file}}" accept="video/*" hidden
+                    x-ref="fileInput"
+                    @change="if ($refs.fileInput.files.length) {
+                    lastVideoUrl = URL.createObjectURL($refs.fileInput.files[$refs.fileInput.files.length - 1]);
+                    }"
+                    @foo.window="lastVideoUrl=null"
+                    />
                     <div class="flex w-[100%] h-[50px] px-2 flex-col bg-[blue] rounded-full shadow text-[white] text-[14px] font-semibold leading-4 items-center justify-center cursor-pointer focus:outline-none">Add Video</div>
                   </label>
             </div>
         
+
+           <!-- ✅ Aperçu de la dernière vidéo sélectionnée -->
+    <template x-if="lastVideoUrl">
+        <div class="mt-4 w-full">
+            <span class="text-green-600 font-semibold">Dernière vidéo ajoutée :</span>
+            <video class="mt-2 rounded w-full max-w-[400px] border shadow" controls :src="lastVideoUrl"></video>
+        </div>
+    </template> 
            
             @foreach ($saphirMultipleFiles[$file] as $key => $item)
-           <div class="flex bg-[#222] text-white border-[1px] border-[grey] mt-[10px]
+           <div class="flex bg-[#222] text-white border-[2px] border-[grey] mt-[10px]
           p-[10px] pb-[20px] pt-[20px] rounded-[5px]">
                 <div class="w-full">
                     @if ($item) 
@@ -55,8 +69,9 @@
                  </span>
              </div>
         </div>
-         @endforeach
+        @endforeach
         @endif
+
 
          @error("saphirMultipleFileErrors.$file")
          <div class="text-[red] pt-[5px]">
