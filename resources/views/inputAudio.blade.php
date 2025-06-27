a5402023dfa4bb71157de2a7fc15da2b85ab514b
yaziddouassi <37118932+yaziddouassi@users.noreply.github.com>
2025-06-27 17:45:51 +0200

    Update inputAudio.blade.php

--- resources/views/inputAudio.blade.php
+++ resources/views/inputAudio.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full"
-            x-data="{ isUploading: false, progress: 0 }"
+            x-data="{ isUploading: false, progress: 0 , audioPreviewUrl: null}"
             x-on:livewire-upload-start="isUploading = true"
             x-on:livewire-upload-finish="isUploading = false"
             x-on:livewire-upload-error="isUploading = false"
@@ -13,23 +13,42 @@
         
             <div class="w-[100%]  flex items-center justify-center">
                 <label class="w-[100%]">
-                    <input type="file" wire:model="saphirFiles.{{$file}}"  accept="audio/*" hidden />
+                    <input type="file" wire:model="saphirFiles.{{$file}}"  accept="audio/*" hidden
+                     x-ref="audioInput"
+                   @change="if ($refs.audioInput.files.length) {
+                    audioPreviewUrl= URL.createObjectURL($refs.audioInput.files[0]);
+                   }"
+                  @foo.window="audioPreviewUrl=null"
+                    />
                     <div class="flex w-[100%] h-[50px] px-2 flex-col bg-[blue] rounded-full shadow text-[white] text-[14px] font-semibold leading-4 items-center justify-center cursor-pointer focus:outline-none">Choose Audio</div>
                   </label>
             </div>
+
+
+            <!-- ✅ Aperçu audio du dernier fichier sélectionné -->
+    <template x-if="audioPreviewUrl">
+        <div class="mt-4 w-full">
+            <span class="text-green-600 font-semibold">Dernier audio ajouté :</span>
+            <audio class="mt-2 w-full" controls :src="audioPreviewUrl"></audio>
+        </div>
+    </template>
+
 
             <div x-show="isUploading">
               <progress max="100" x-bind:value="progress" class="h-[10px] bg-[blue] rounded-[5px]"></progress>
           </div>
 
-            @if ($saphirFiles[$file])
-            <div class="pt-[5px]">
-              <figure>
-                <audio controls class="pt-[8px]  w-full" src="{{ $saphirFiles[$file]->temporaryUrl()}}">
-                </audio>
-               </figure>
+            
+
+         <div class="pt-[5px]">
+           @if ($saphirFiles[$file])
+              <div class="bg-[#DDD] text-black border-[2px] border-white mt-[10px]
+               p-[10px] pb-[20px] pt-[20px] rounded-[5px]">
+            {{ $saphirFiles[$file]->getClientOriginalName() }}
             </div>
-            @endif
+           @endif
+         </div>
+
 
             @error("saphirFiles.$file")
               <div class="text-[red] pt-[5px]">
