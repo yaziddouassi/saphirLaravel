1def4be263b3ad3f4d75749d79f5a9965581e509
yaziddouassi <37118932+yaziddouassi@users.noreply.github.com>
2025-12-15 20:29:05 +0100

    Update topBar.blade.php

--- resources/views/topBar.blade.php
+++ resources/views/topBar.blade.php
@@ -1,8 +1,8 @@
 <div class="max-[799px]:hidden pt-[2px] pl-[2px] pr-[2px] ">
 
-   <div class="h-[60px] bg-[BLACK] text-white grid grid-cols-2">
+   <div class="h-[60px] bg-[#ddd] text-white grid grid-cols-2">
         
-      <div class="bg-[greee] flex pl-[5px] pt-[12px] text-[20px]">
+      <div class="text-[darkblue] flex pl-[5px] pt-[12px] text-[20px]">
          <span class="material-icons text-[30px] pt-[2px]" style="display:block">
             account_circle
             </span>
@@ -14,11 +14,11 @@
 
       <div class="text-right pt-[8px] pr-[4px]">
          <a href="/admin/crud-generator" wire:navigate >
-         <button class="bg-[blue] border border-white w-[80px] rounded-[3px] p-[7px] text-[20px]">
+         <button class="bg-[darkblue] border border-white w-[80px] rounded-[3px] p-[7px] text-[20px]">
             CRUD
          </button></a>&nbsp;
          <a href="/admin/chart-generator" wire:navigate >
-         <button class="border border-white  w-[90px] rounded-[3px] p-[7px] text-[20px]">
+         <button class="border border-[darkblue] text-[darkblue] w-[90px] rounded-[3px] p-[7px] text-[20px]">
               <span>
                CHARTS
             </span>  
