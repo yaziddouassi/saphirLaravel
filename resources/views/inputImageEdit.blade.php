fa113ed22c171dbf5a6b2e2f34ba84240ecbd3a7
yaziddouassi <37118932+yaziddouassi@users.noreply.github.com>
2025-12-21 21:17:41 +0100

    Update inputImageEdit.blade.php

--- resources/views/inputImageEdit.blade.php
+++ resources/views/inputImageEdit.blade.php
@@ -1,74 +1,101 @@
 <div class="w-full"
-    x-data="{ isUploading: false, progress: 0 , showFile : true, imagePreviewUrl: null}"
-    x-on:livewire-upload-start="isUploading = true"
-    x-on:livewire-upload-finish="isUploading = false"
-    x-on:livewire-upload-error="isUploading = false"
-    x-on:livewire-upload-progress="progress = $event.detail.progress">
+            x-data="{ 
+                isUploading: false, 
+                progress: 0, 
+                videoPreviewUrl: null,
+                hasNewUpload: false,
+                cle : '{{$file}}',
+                oldVideoUrl: '',
 
-    <div class="mb-[5px]">
-        <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
-     </div>
-
-    <div class="w-[100%]  flex items-center justify-center" x-show="$wire.saphirFile0pens.{{$file}}">
-        <label class="w-[100%]">
-            <input type="file" wire:model="saphirFiles.{{$file}}" accept="image/png,image/jpeg" hidden 
-            x-ref="imageInput"
-                   @change="if ($refs.imageInput.files.length) {
-                    imagePreviewUrl= URL.createObjectURL($refs.imageInput.files[0]);
-                   }"
-                  @foo.window="imagePreviewUrl=null" 
-            />
-            <div class="flex w-[100%] h-[50px] px-2 flex-col bg-[blue] rounded-full shadow text-[white] text-[14px] font-semibold leading-4 items-center justify-center cursor-pointer focus:outline-none">Choose Image</div>
-          </label>
-    </div>
-
-
-     <template x-if="imagePreviewUrl">
-        <div class="mt-4">
-            <span class="text-green-600 font-semibold">Dernière image ajoutée :</span>
-            <img :src="imagePreviewUrl" alt="Dernière image" class="mt-2 rounded w-[150px] h-auto border shadow">
-        </div>
-    </template>
-
-     <!-- Progress Bar -->
-     <div x-show="isUploading">
-        <progress max="100" x-bind:value="progress" class="h-[10px] bg-[blue] rounded-[5px]"></progress>
-    </div>
-    
-    
-    <div class="mt-[-4px]  min-h-[50px]"  x-show="!$wire.saphirFile0pens.{{$file}}">
-    
-        <div class="text-right relative text-[red] text-[32px] font-[arial] pr-[5px] z-[100]">
-           <span @click="$wire.saphirFile0pens.{{$file}}=true" class="cursor-pointer">C</span><span @click="$wire.saphirFile0pens.{{$file}}=true" class="text-[#CCC] cursor-pointer">lo</span ><span @click="$wire.saphirFile0pens.{{$file}}=true" class="cursor-pointer">se</span>
-        </div>
-        <div class="mt-[-44px]">
-            @if ($saphirRecord != null)
-            <div class="border-[1px] border-black">
-            <img src="{{$saphirUrlStorage}}{{ $saphirRecord[$file]}}" width="100%" 
-            class="min-h-[50px] max-h-[50vh]"
-            alt="Image not found!"> 
+                init() {
+                  console.log($wire.saphirRecordBis)
+                  this.updateOldVideoUrl();
+                  
+                  // Écouter les changements de Livewire
+                  this.$watch('$wire.saphirRecordBis', () => {
+                      this.updateOldVideoUrl();
+                  });
+                  
+                  this.$watch('$wire.saphirUrlStorage', () => {
+                      this.updateOldVideoUrl();
+                  });
+                },
+                
+                updateOldVideoUrl() {
+                    if ($wire.saphirRecordBis && $wire.saphirRecordBis[this.cle]) {
+                        this.oldVideoUrl = $wire.saphirUrlStorage + $wire.saphirRecordBis[this.cle];
+                        console.log('URL mise à jour:', this.oldVideoUrl);
+                    }
+                }
+            }"
+            x-on:livewire-upload-start="isUploading = true"
+            x-on:livewire-upload-finish="isUploading = false; $wire.saphirHasNewUpload[cle] = true"
+            x-on:livewire-upload-error="isUploading = false"
+            x-on:livewire-upload-progress="progress = $event.detail.progress"
+            >
+          
+            <div class="mb-[5px]">
+                <span class="text-[darkblue] font-bold">{{$label}}</span><span class="text-[red]">@if($required==true)*@endif</span> 
             </div>
+        
+            <!-- Bouton d'upload -->
+            <div class="w-[100%] flex items-center justify-center">
+                <label class="w-[100%]">
+                    <input type="file" 
+                           wire:model="saphirFiles.{{$file}}" 
+                           accept="image/png,image/jpeg"
+                           hidden 
+                           x-ref="videoInput"
+                           @change="if ($refs.videoInput.files.length) {
+                               $wire.saphirPreviewUrl[cle] = URL.createObjectURL($refs.videoInput.files[0]);
+                               $wire.saphirHasNewUpload[cle] = true;
+                           }"
+                    />
+                    <div class="flex w-[100%] h-[50px] px-2 flex-col border-[1px] border-black rounded-full 
+                    shadow text-black text-[14px] font-semibold leading-4 items-center justify-center
+                    cursor-pointer focus:outline-none">
+                        Choisir une image
+                    </div>
+                </label>
+            </div>
+        
+            @if ($saphirFiles[$file])
+            <!-- Aperçu de la nouvelle vidéo avec bouton effacer -->
+            <template x-if="$wire.saphirPreviewUrl[cle] && $wire.saphirHasNewUpload[cle]">
+                <div class="mt-4 w-full">
+                    <img class="mt-2 w-full max-w-[500px] max-h-[50vh] rounded shadow border" :src="$wire.saphirPreviewUrl[cle]"/>
+                </div>
+            </template>
             @endif
 
-        </div>  
-      </div>
 
+            <!-- Progress Bar -->
+            <div x-show="isUploading" class="mt-2">
+                <progress max="100" x-bind:value="progress" class="w-full h-[10px] bg-[blue] rounded-[5px]"></progress>
+            </div>
 
-    <div class="pt-[5px]" x-show="$wire.saphirFile0pens.{{$file}}">
-                  @if ($saphirFiles[$file])
-                   <div class="bg-[#DDD] text-black border-[2px] border-white mt-[10px]
-                   p-[10px] pb-[20px] pt-[20px] rounded-[5px]">
-                  {{ $saphirFiles[$file]->getClientOriginalName() }}
-                  </div>
-              @endif
-             </div>
-    
+            <!-- Affichage de l'ancienne vidéo (seulement si pas de nouveau upload) -->
+            @if ($saphirRecord != null && isset($saphirRecord[$file]))
+            <template x-if="!$wire.saphirPreviewUrl[cle] && !$wire.saphirHasNewUpload[cle] && oldVideoUrl">
+                <div class="mt-[5px]">
+                     <img x-bind:src="oldVideoUrl"
+                          class="mt-2 w-full max-w-[500px] max-h-[50vh] rounded shadow border" />
+                </div>
+            </template>
+            @endif
 
-    @error("saphirFiles.$file")
-    <div class="text-[red] pt-[5px]">
-         <span class="error">{{ $message }}</span> 
-    </div>
-    @enderror
-  
-   
+            <!-- Nom du fichier uploadé -->
+            <div class="pt-[5px]" x-show="$wire.saphirPreviewUrl[cle]">
+                @if ($saphirFiles[$file])
+                    <div class="bg-[#DDD] text-black border-[2px] border-white mt-[10px] p-[10px] pb-[20px] pt-[20px] rounded-[5px]">
+                        {{ $saphirFiles[$file]->getClientOriginalName() }}
+                    </div>
+                @endif
+            </div>
+
+            @error("saphirFiles.$file")
+                <div class="text-[red] pt-[5px]">
+                    <span class="error">{{ $message }}</span> 
+                </div>
+            @enderror
 </div>
